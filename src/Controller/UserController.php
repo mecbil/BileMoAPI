754239c3e26cf07c7d927012e66c6ce564d8ce5f
Nabil Mecili <nabilmecili@gmail.com>
2022-03-20 17:27:36 +0100

    Add somes responses on product entity

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,7 +14,14 @@ class UserController extends AbstractController
      * 
      * @OA\Tag(name="Utilisateur")
      * @OA\Tag(name="Client")
-     * 
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne la liste des produits",
+     *)
+     * @OA\Response(
+     *     response=500,
+     *     description="Retourne une erreur serveur",
+     *)
      * @Route("/api/products", name="app_products", methods={"GET"})
      */
     public function showAllProduct(ProductsRepository $productsRepository): Response
@@ -31,6 +38,14 @@ class UserController extends AbstractController
      * @OA\Tag(name="Utilisateur")
      * @OA\Tag(name="Client")
      * 
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne le détail d'un produit",
+     *)
+     * @OA\Response(
+     *     response=404,
+     *     description="Retourn Produit non trouvé, ou pas de route si l'Id n'est pas donné",
+     *)
      * @Route("/api/product/{id}", name="app_product", methods={"GET"})
      */
     public function sowOneProduct($id, ProductsRepository $product): Response
