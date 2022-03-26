cf5c9deeba1653dfd163b366071b56e987fc7026
Nabil Mecili <nabilmecili@gmail.com>
2022-03-26 10:09:07 +0100

    Add "addUser" method

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
 use OpenApi\Annotations as OA;
 use App\Repository\UsersRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ClientController extends AbstractController
 {
@@ -38,5 +44,51 @@ class ClientController extends AbstractController
         $response = $this->json($product, 200, [],[]);
 
         return $response;
+    }
+
+    /**
+     * 
+     * @OA\Tag(name="Client")
+     * 
+     * 
+     * @Route("/api/user/add", name="app_user_add", methods={"POST"})
+     */
+    public function addUser(Request $request, SerializerInterface $serializer, 
+    EntityManagerInterface $em, UserPasswordEncoderInterface $encoder, ValidatorInterface $validator): Response
+    {
+        // Obtenir les informations
+        $jsonRecu = $request->getContent();
+
+        try{
+
+        // Deserializer les informations
+        $user = $serializer->deserialize($jsonRecu, Users::class, 'json');
+
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+            return $this->json($errors, 400);
+        }
+
+        // encoder le mot de pass
+        $encoded = $encoder->encodePassword($user, $user->getPassword());
+        $user->setPassword($encoded);
+
+        // enregistrer dans la BD
+        $em->persist($user);
+        $em->flush();
+
+        // Envoyer la reponse (cas  valide)
+        $response = $this->json('Utilisateur ajouté avec succès', 201, [],[]);
+
+        return $response;
+
+        } catch(\Exception $e) {
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ], 400);
+        }
+
     }
 }
